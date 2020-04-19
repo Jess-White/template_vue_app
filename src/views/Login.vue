f60f0ec409d1bd3f9729a523552e73a3ecb6dc74
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2020-04-19 13:07:07 -0500

    added login page

---
+++ src/views/Login.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="login">
+
+    <div class="row">
+      <form class="col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-sm-10 offset-sm-1" v-on:submit.prevent="submit()">
+        <h1 class="text-center mb-5">Login</h1>
+        <ul>
+          <li class="text-danger" v-for="error in errors">{{ error }}</li>
+        </ul>
+        <div class="form-group">
+          <label>Email:</label>
+          <input type="email" class="form-control" v-model="email">
+        </div>
+        <div class="form-group">
+          <label>Password:</label>
+          <input type="password" class="form-control" v-model="password">
+        </div>
+        <input type="submit" class="btn btn-primary" value="Submit">
+      </form>
+
+      <router-link to="/users/new">Add New User</router-link></li>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      email: "",
+      password: "",
+      errors: []
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        email: this.email,
+        password: this.password
+      };
+      axios
+        .post("/api/sessions", params)
+        .then(response => {
+          axios.defaults.headers.common["Authorization"] =
+            "Bearer " + response.data.jwt;
+          localStorage.setItem("jwt", response.data.jwt);
+
+          this.$parent.userEmail = response.data.email;
+          localStorage.setItem("userEmail", response.data.email);
+
+
+          this.$router.push("/dashboard");
+        })
+        .catch(error => {
+          this.errors = ["Invalid email or password."];
+          this.email = "";
+          this.password = "";
+        });
+    }
+  }
+};
+</script>
