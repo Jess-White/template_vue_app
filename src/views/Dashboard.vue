f7e1d9e7135bbee5adb0b11ee0505ef90c8ca816
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2020-04-19 14:06:36 -0500

    added dashboard

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="dashboard">
+    <h1>{{ message }}</h1>
+
+    <router-link to="/letters/new">Start New Letter</router-link></li>
+
+    <router-link to="/letters/index">View Saved Letters</router-link></li>
+  </div>
+
+
+
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  data: function() {
+    return {
+      message: "View saved letters, or start a new letter!"
+    };
+  },
+  created: function() {},
+  methods: {}
+};
+</script>
